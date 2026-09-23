4231a1ef45ec63253762dec464c21213afea0685
PowerDuckie <powerduckie.team@gmail.com>
2026-09-23 23:38:34 +0800

    chore: stop tracking generated docusaurus cache

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/electron/powerduck-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/electron/powerduck-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/electron/powerduck-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/electron/powerduck-docs/src/css/custom.css"),
-];
